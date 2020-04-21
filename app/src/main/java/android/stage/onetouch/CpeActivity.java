2db3f47513234a4873666a018c75d8828d75daab
Marco Poveromo <m.poveromo@campus.unimib.it>
2020-04-21 09:12:04 +0200

    Nuova creazione dell'app con un'altra libreria, contiene solo la console e utilizza Service

---
+++ app/src/main/java/android/stage/onetouch/CpeActivity.java
@@ -0,0 +1,23 @@
+package android.stage.onetouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class CpeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cpe);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = new CpeConsoleFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
