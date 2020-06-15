c6f6252e81d3b6b59b26ed1db8ec0ea51fabee79
Zorzin Andrea <andrea.zorzin@emaze.net>
2020-06-15 09:13:27 +0200

    Merged heads

--- app/src/main/java/android/stage/onetouch/view/MainActivity.java
+++ app/src/main/java/android/stage/onetouch/view/MainActivity.java
@@ -3,11 +3,10 @@ package android.stage.onetouch.view;
 import android.graphics.Color;
 import android.os.Bundle;
 
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
 import android.stage.onetouch.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.helmet);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setSubtitle(R.string.app_subtitle);
+
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container_main);
-        if(fragment == null) {
+        if (fragment == null) {
             fragment = new HomePageFragment();
             fm.beginTransaction().add(R.id.fragment_container_main, fragment).commit();
         }
